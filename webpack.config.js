e5e246d75bbd3391c584331907721b6de2df615f
plus-minus <blue.box@o2.pl>
2019-10-04 09:37:10 +0200

    Initial structure

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./js/app.js",
+  output: {
+    filename: "js/out.js",
+    path: path.resolve(__dirname, ".")
+  },
+  watch: false,
+  mode: "development",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      }
+    ]
+  }
+};
